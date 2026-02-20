cd25a58aa3bd0c58fe0b41c12638cf1951a530d4
LAÍS ZANQUETA <lais.zanqueta@portalsesisp.org.br>
2026-02-20 14:47:33 -0300

    aula pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import PHome from "./pages/PHome/PHome";
+import PServicos from "./pages/PServicos/PServicos";
+import PContatos from "./pages/PContatos/PContatos";
 
 function App() {
   return (
-    <>
-    <PHome />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PHome />} />
+        <Route path="/servicos" element={<PServicos />} />
+        <Route path="/contatos" element={<PContatos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/PServicos/PServicos.tsx
@@ -0,0 +1,18 @@
+import Navegacao from "../../components/Navegacao/Navegacao"
+
+import BoasVindas from "../../components/BoasVindas/BoasVindas"
+
+import Rodape from "../../components/Rodape/Rodape"
+
+
+function PHome() {
+    return (
+        <>
+        <Navegacao />
+        <BoasVindas />
+        <Rodape />
+        </>
+    )
+}
+
+export default PHome
